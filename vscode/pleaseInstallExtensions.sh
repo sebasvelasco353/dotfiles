c5244dd964c09434ecdfe8380af5284a1e5d5991
sebastian velasco <11669998+sebasvelasco353@users.noreply.github.com>
2024-08-15 21:52:10 -0500

    modified vscode settings and shell script

--- vscode/pleaseInstallExtensions.sh
+++ vscode/pleaseInstallExtensions.sh
@@ -1,54 +1,25 @@
 #!/bin/bash
 
-echo "Hello, i will be installing some vscode extensions"
-
-echo "dbaeumer.vscode-eslint"
+echo "0. Installing VSCode Extensions"
 code --install-extension dbaeumer.vscode-eslint
-
-echo "formulahendry.auto-rename-tag"
 code --install-extension formulahendry.auto-rename-tag
-
-echo "github.github-vscode-theme"
 code --install-extension github.github-vscode-theme
-
-echo "golang.go"
 code --install-extension golang.go
-
-echo "gruntfuggly.todo-tree"
 code --install-extension gruntfuggly.todo-tree
-
-echo "kamikillerto.vscode-colorize"
 code --install-extension kamikillerto.vscode-colorize
-
-echo "ms-python.debugpy"
 code --install-extension ms-python.debugpy
-
-echo "ms-python.python"
 code --install-extension ms-python.python
-
-echo "ms-python.vscode-pylance"
 code --install-extension ms-python.vscode-pylance
-
-echo "pkief.material-icon-theme"
 code --install-extension pkief.material-icon-theme
-
-echo "ritwickdey.liveserver"
 code --install-extension ritwickdey.liveserver
-
-echo "sdras.vue-vscode-snippets"
 code --install-extension sdras.vue-vscode-snippets
-
-echo "sirtori.indenticator"
 code --install-extension sirtori.indenticator
-
-echo "usernamehw.errorlens"
 code --install-extension usernamehw.errorlens
-
-echo "vscodevim.vim"
 code --install-extension vscodevim.vim
-
-echo "vue.volar"
 code --install-extension vue.volar
-
-echo "wix.vscode-import-cost"
 code --install-extension wix.vscode-import-cost
+
+echo "1. Deleting old Config"
+rm Library/Application\ Support/Code/User/
+echo "2. Creating SymLink fo VSCode's configuration"
+ln -s ./settings.json ~/Library/Application\ Support/Code/User/
