302f732e6fa6bd2ab55b194c3db05c1ddb422881
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2024-12-01 17:33:50 -0500

    Added default Langs to the config

---
+++ nvim/lua/config/langs.lua
@@ -0,0 +1,11 @@
+return {
+  -- Langs
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.vue" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.go" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  -- Lang related utils
+  { import = "lazyvim.plugins.extras.linting.eslint" },
+}
