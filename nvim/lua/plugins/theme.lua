d2f811a188de7d4558a82932cd0226efab0296e0
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2024-12-01 21:22:46 -0500

    Added two plugins: css color viewer and the nav bar to see where am i in the file

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,7 +1,11 @@
 return {
   -- Add nordic theme
+  { "AlexvZyl/nordic.nvim" },
+  -- Show css colorscheme
   {
-    "AlexvZyl/nordic.nvim",
+    "NvChad/nvim-colorizer.lua",
+    event = "BufReadPre",
+    opts = {},
   },
   -- Use Nordic Theme
   {
@@ -17,11 +21,33 @@ return {
     opts = {
       dashboard = {
         preset = {
-          header = [[
-Welcome back friend
-]],
+          header = [[ Welcome back friend ]],
         },
       },
+    },
+  },
+
+  -- ScrollView (scrollbar and indicators)
+  {
+    "dstein64/nvim-scrollview",
+    config = function(_, opts)
+      require("scrollview").setup(opts)
+      require("scrollview.contrib.gitsigns").setup(opts)
+
+      vim.g.scrollview_excluded_filetypes = { "neo-tree", "nerdtree" }
+      vim.g.scrollview_current_only = true
+      vim.g.column = 120
+      vim.g.scrollview_signs_on_startup =
+        { "diagnostics", "marks", "search", "conflicts", "cursor", "search", "textwidth" }
+      vim.g.scrollview_diagnostics_severities = {
+        vim.diagnostic.severity.ERROR,
+        -- vim.diagnostic.severity.INFO,
+        -- vim.diagnostic.severity.HINT,
+        -- vim.diagnostic.severity.WARN,
+      }
+    end,
+    opts = {
+      always_show = true,
     },
   },
 }
