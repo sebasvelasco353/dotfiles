c7b673ff0963242cf5a1778e74a5436f8ea7c29d
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2024-12-04 20:33:20 -0500

    Obsidian config

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -14,6 +14,28 @@ return {
         path = "~/Documents/notes/",
       },
     },
+
+    templates = {
+      folder = "/templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      -- A map for custom variables, the key should be the variable and the value a function
+      substitutions = {},
+    },
+
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "/dailies",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-%m-%d",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { "daily", "todo" },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = "/daily.md",
+    },
+
     -- customize how note IDs are generated given an optional title.
     ---@param title string|?
     ---@return string
@@ -30,7 +52,15 @@ return {
           suffix = suffix .. string.char(math.random(65, 90))
         end
       end
-      return tostring(os.date("%Y%m%d:%H%M%S")) .. "-" .. suffix
+      return suffix .. "-" .. tostring(os.date("%Y%m%d:%H%M%S"))
     end,
+  },
+
+  -- Define your own callbacks to further customize behavior.
+  callbacks = {
+    -- Runs right before writing the buffer for a note.
+    ---@param client obsidian.Client
+    ---@param note obsidian.Note
+    pre_write_note = function(client, note) end,
   },
 }
