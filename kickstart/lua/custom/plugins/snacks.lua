3e381b63550d6be28e084dfedf69e7b77f7892a0
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2025-04-11 23:11:24 -0500

    feat(kickstart): Added Snacks for QoL plugins

---
+++ kickstart/lua/custom/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  -- Snacks.nvim config (homepage)
+  {
+    'folke/snacks.nvim',
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[ Welcome back friend ]],
+        },
+      },
+    },
+  },
+}
