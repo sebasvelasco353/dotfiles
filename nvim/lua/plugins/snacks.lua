d5d5a97a76925be34bdc70b1f85146f315d053b5
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2025-10-02 12:45:27 -0500

    Added obsidian for note taking in nvim, no need to exit the flow when taking notes

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -14,27 +14,14 @@ return {
     picker = {
       sources = {
         explorer = {
-          -- Show hidden Files
-          files = {
-            hidden = true,
-          },
+          -- Show ignored and hidden Files
+          ignored = true,
+          hidden = true,
           -- Move explorer to a floating window with preview
           auto_close = true,
           layout = {
-            { preview = true },
             layout = {
-              box = "horizontal",
-              width = 0.8,
-              height = 0.8,
-              {
-                box = "vertical",
-                border = "rounded",
-                title = "{source} {live} {flags}",
-                title_pos = "center",
-                { win = "input", height = 1, border = "bottom" },
-                { win = "list", border = "none" },
-              },
-              { win = "preview", border = "rounded", width = 0.7, title = "{preview}" },
+              position = "right",
             },
           },
         },
