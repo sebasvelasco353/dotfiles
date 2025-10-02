9b6a1dd222be876053aade36b369c6f441205831
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2025-10-02 18:34:15 -0500

    cleanup i guess

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -3,21 +3,22 @@ return {
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = false,
   ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
   dependencies = {
-    -- Required.
     "nvim-lua/plenary.nvim",
-
-    -- see below for full list of optional dependencies 👇
+    -- "nvim-telescope/telescope.nvim",
   },
   opts = {
+    -- picker = {
+    --   name = "telescope.nvim",
+    --   note_mappings = {
+    --     new = "<C-x>",
+    --     insert_link = "<C-l>",
+    --   },
+    --   tag_mappings = {
+    --     tag_note = "<C-x>",
+    --     insert_tag = "<C-l>",
+    --   },
+    -- },
     workspaces = {
       {
         name = "personal",
