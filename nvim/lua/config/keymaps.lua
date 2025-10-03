0ccdb86bc77025dcb9ef5056ef99d870dd06d62a
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2025-10-02 20:06:16 -0500

    Added keymap for moving lines up and down

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+local opts = { noremap = true, silent = true }
+
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-M-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-M-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-M-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-M-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- Normal mode: Move current line up/down
+map("n", "<S-Up>", ":m .-2<CR>==", opts) -- Alt + Up
+map("n", "<S-Down>", ":m .+1<CR>==", opts) -- Alt + Down
+
+-- Visual mode: Move selection up/down
+map("v", "<S-Up>", ":m '>+1<CR>gv=gv", opts)
+map("v", "<S-Down>", ":m '<-2<CR>gv=gv", opts)
