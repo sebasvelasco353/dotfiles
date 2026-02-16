4737c677a5c5c51a17df53d2b71136c6f0e2b68b
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2026-02-16 17:35:57 -0500

    going back to nvim maybe????

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,12 +1,13 @@
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
+  { "shaunsingh/nord.nvim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "nord",
     },
   },
 }
