2388ee1eb091caef00ec502b65888b3751e9f7d0
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2026-02-15 21:40:21 -0500

    restarted nvim configuration, back to basics :V

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,12 +1,6 @@
 local map = LazyVim.safe_keymap_set
 local opts = { noremap = true, silent = true }
 
--- Resize window using <ctrl> arrow keys
-map("n", "<C-M-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
-map("n", "<C-M-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
-map("n", "<C-M-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
-map("n", "<C-M-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
-
 -- Normal mode: Move current line up/down
 map("n", "<S-Up>", ":m .-2<CR>==", opts) -- Alt + Up
 map("n", "<S-Down>", ":m .+1<CR>==", opts) -- Alt + Down
